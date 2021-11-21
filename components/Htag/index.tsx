a78dfdfc9a35e36ed5ae0d033e8f061bf7691e1c
Igor Trifonov <Ha1lowe3n@yandex.ru>
2021-11-21 17:20:30 +0800

    create component Htag, delete trash, add fonts

---
+++ components/Htag/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import styles from "./Htag.module.scss";
+
+interface HtagProps {
+    tag: "h1" | "h2" | "h3";
+    children: React.ReactNode;
+}
+
+export const Htag: React.FC<HtagProps> = ({ tag, children }) => {
+    return (
+        <div className={styles.tags}>
+            {tag === "h1" && <h1>{children}</h1>}
+            {tag === "h1" && <h2>{children}</h2>}
+            {tag === "h1" && <h3>{children}</h3>}
+        </div>
+    );
+};
