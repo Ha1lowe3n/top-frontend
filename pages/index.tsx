896c0dc4d288509cee50fdc9e8757487faeee6f9
Igor Trifonov <Ha1lowe3n@yandex.ru>
2022-05-01 21:28:07 +0800

    reset app, start again

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,6 @@
 import type { NextPage } from "next";
 import React from "react";
 import { useEffect, useState } from "react";
-import { Button } from "../components/Button";
-import { Htag } from "../components/Htag";
-import { Paragraph } from "../components/Paragraph";
-import { Rating } from "../components/Rating";
-import { Tag } from "../components/Tag";
-import { withLayout } from "../layouts/Layout";
 
 const Home: NextPage = () => {
     const [counter, setCounter] = useState<number>(0);
@@ -18,28 +12,5 @@ const Home: NextPage = () => {
         };
     }, [counter]);
 
-    return (
-        <div>
-            <Htag tag="h1">Текст</Htag>
-            <Button color="primary">Узнать подрпобнее</Button>
-            <Button color="ghost" arrow="right">
-                Читатиь отзывы
-            </Button>
-            <Paragraph size={"big"}>Hello</Paragraph>
-            <Paragraph size={"medium"}>Hello</Paragraph>
-            <Paragraph size={"low"}>Hello</Paragraph>
-            <Tag color="primary" href="#" size="small">
-                привет
-            </Tag>
-            <Tag color="primary" href="#" size="big">
-                привет
-            </Tag>
-            <Tag color="red" href="#" size="big">
-                привет
-            </Tag>
-            <Rating isEditable />
-        </div>
-    );
+    return <div></div>;
 };
-
-export default withLayout(Home);
