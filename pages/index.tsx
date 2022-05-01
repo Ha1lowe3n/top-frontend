62b74e72f910fb8e4a7c3f6820e174acdaae4fc6
Igor Trifonov <Ha1lowe3n@yandex.ru>
2022-05-01 21:49:43 +0800

    add component Htag

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,14 @@
 import type { NextPage } from "next";
 import React from "react";
-import { useEffect, useState } from "react";
+
+import { Htag } from "../components";
 
 const Home: NextPage = () => {
-    const [counter, setCounter] = useState<number>(0);
-
-    useEffect(() => {
-        console.log(counter);
-        return () => {
-            console.log("unmount");
-        };
-    }, [counter]);
-
-    return <div></div>;
+    return (
+        <div>
+            <Htag tag="h1">Hello</Htag>
+        </div>
+    );
 };
+
+export default Home;
