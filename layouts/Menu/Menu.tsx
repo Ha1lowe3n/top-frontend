ce1070b43d42ec449c0edf1a99a6ebf216650fd8
Igor Trifonov <Ha1lowe3n@yandex.ru>
2022-05-05 02:35:34 +0800

    add router for show correct items in menu

--- layouts/Menu/Menu.tsx
+++ layouts/Menu/Menu.tsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
 import Link from 'next/link';
 import clsx from 'clsx';
+import { useRouter } from 'next/router';
 
 import styles from './Menu.module.scss';
 import { BookIcon, BoxIcon, CloudIcon, HatIcon } from './icons';
@@ -17,57 +18,78 @@ const firstLevelMenu: FirstLevelMenuItem[] = [
 
 export const Menu: React.FC = () => {
 	const { menu, setMenu, firstCategory } = useContext(AppContext);
+	const router = useRouter();
+
+	const currentAlias = router.asPath.split('/')[2];
+
+	const changeSecondCategory = (secondCategory: string) => {
+		setMenu &&
+			setMenu(
+				menu.map((m) => {
+					if (m._id.secondCategory === secondCategory) {
+						m.isOpened = !m.isOpened;
+					}
+					return m;
+				}),
+			);
+	};
 
 	const buildFirstLevel = () => {
-		return (
-			<>
-				{firstLevelMenu.map((m) => (
-					<div key={m.route}>
-						<Link href={`/${m.route}`}>
-							<a>
-								<div
-									className={clsx(styles.firstLevel, {
-										[styles.firstLevelActive]: m.id === firstCategory,
-									})}
-								>
-									{m.icon}
-									<span>{m.name}</span>
-								</div>
-							</a>
-						</Link>
+		return firstLevelMenu.map((m) => (
+			<div key={m.route}>
+				<Link href={`/${m.route}`}>
+					<a>
+						<div
+							className={clsx(styles.firstLevel, {
+								[styles.firstLevelActive]: m.id === firstCategory,
+							})}
+						>
+							{m.icon}
+							<span>{m.name}</span>
+						</div>
+					</a>
+				</Link>
 
-						{m.id == firstCategory && buildSecondLevel(m)}
-					</div>
-				))}
-			</>
-		);
+				{m.id == firstCategory && buildSecondLevel(m)}
+			</div>
+		));
 	};
 
 	const buildSecondLevel = (menuItem: FirstLevelMenuItem) => {
 		return (
 			<div className={styles.secondBlock}>
-				{menu.map((m) => (
-					<div key={m._id.secondCategory}>
-						<div className={styles.secondLevel}>{m._id.secondCategory}</div>
-						<div
-							className={clsx(styles.secondLevelBlock, {
-								[styles.secondLevelBlockOpened]: m.isOpened,
-							})}
-						>
-							{buildThirdLevel(m.pages, menuItem.route)}
+				{menu.map((m) => {
+					if (m.pages.map((p) => p.alias).includes(currentAlias)) {
+						m.isOpened = true;
+					}
+					return (
+						<div key={m._id.secondCategory}>
+							<div
+								className={styles.secondLevel}
+								onClick={() => changeSecondCategory(m._id.secondCategory)}
+							>
+								{m._id.secondCategory}
+							</div>
+							<div
+								className={clsx(styles.secondLevelBlock, {
+									[styles.secondLevelBlockOpened]: m.isOpened,
+								})}
+							>
+								{buildThirdLevel(m.pages, menuItem.route)}
+							</div>
 						</div>
-					</div>
-				))}
+					);
+				})}
 			</div>
 		);
 	};
 
 	const buildThirdLevel = (pages: PageItem[], route: string) => {
 		return pages.map((p) => (
-			<Link href={`/${route}/${p.alias}`}>
+			<Link key={p.alias} href={`/${route}/${p.alias}`}>
 				<a
 					className={clsx(styles.thirdLevel, {
-						[styles.thirdLevelActive]: false,
+						[styles.thirdLevelActive]: p.alias === currentAlias,
 					})}
 				>
 					{p.category}
@@ -76,5 +98,5 @@ export const Menu: React.FC = () => {
 		));
 	};
 
-	return <div className={styles.menu}>{buildFirstLevel()}</div>;
+	return <>{buildFirstLevel()}</>;
 };
